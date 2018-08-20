a185d89a93a83f2dcfaa62d796b0fdf711cf385c
KseniaKardash <just.kardash@gmail.com>
2018-08-20 17:29:20 +0300

    Added search by name

--- src/components/screens/PostsFeed/PostsFeed.js
+++ src/components/screens/PostsFeed/PostsFeed.js
@@ -1,6 +1,6 @@
 /* @flow */
 import React, {PureComponent} from 'react';
-import {View, StyleSheet, FlatList} from 'react-native';
+import {View, StyleSheet, FlatList, TextInput} from 'react-native';
 import ButtonIcon from '../../common/ButtonIcon';
 import HeaderTitle from '../../common/HeaderTitle';
 import UserPost from '../../common/UserPost';
@@ -15,31 +15,57 @@ type Posts = {
   userName: string
 }
 
-class PostsFeed extends PureComponent<Props>{
-  render(){
-      const {posts} = this.props;
-    return (
-    <View style={styles.container}>
+class PostsFeed extends PureComponent<Props> {
+
+  state = {
+    searchName: '',
+    toggleSearch: false
+  };
+
+  filtering = (posts, query) => {
+    return posts.filter((post) => {
+      const userName = post.userName.toUpperCase();
+      const queryData = query.toUpperCase();
+      return userName.indexOf(queryData) > -1;
+    });
+  };
+
+  _setToggle = () => this.setState(state => ({
+    toggleSearch: !state.toggleSearch
+  }));
+
+  render() {
+    let filter;
+    const {posts} = this.props;
+    const {searchName, toggleSearch} = this.state;
+    (searchName !== '')
+      ? filter = this.filtering(posts, searchName)
+      : filter = posts;
+    return (<View style={styles.container}>
       <View style={styles.header}>
-        <ButtonIcon iconName="search"/>
+        <ButtonIcon iconName="search" setToggle={this._setToggle}/>
         <HeaderTitle text="FEED"/>
         <ButtonIcon iconName="plus"/>
       </View>
-      <FlatList
-        removeClippedSubviews={false}
-        disableVirtualization
+      {
+        toggleSearch
+          ? (<TextInput style={styles.textInput}
+            placeholder="Search"
+            onChangeText={searchName => this.setState({searchName})}/>)
+          : (<View/>)
+      }
+      <FlatList removeClippedSubviews={false}
+        disableVirtualization="disableVirtualization"
         showsVerticalScrollIndicator={false}
         initialNumToRender={15}
         maxToRenderPerBatch={10}
-        data={posts}
+        data={filter}
         keyExtractor={item => item._id}
-        renderItem={({item}) =>
-        <View>
-          <UserPost userName={item.userName} uri={{
-              uri: item.uri
-            }} uriPhoto={{
-              uri: item.uriPhoto
-            }}/>
+        renderItem={({item}) => <View>
+          <UserPost
+            userName={item.userName}
+            uri={{ uri: item.uri }}
+            uriPhoto={{ uri: item.uriPhoto }}/>
         </View>}/>
     </View>);
   };
@@ -50,12 +76,16 @@ const styles = StyleSheet.create({
     width: '100%',
     flex: 1,
     padding: 20,
-    paddingBottom: 0,
+    paddingBottom: 0
   },
   header: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between'
+  },
+  textInput: {
+    marginBottom: 10,
+    fontSize: 18
   }
 });
 
