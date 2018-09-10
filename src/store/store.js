bec32bc3bbacf5c2a22245935f38595c2b3bff9d
KseniaKardash <just.kardash@gmail.com>
2018-09-10 13:16:25 +0300

    Added require instead of import

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,11 @@
 /* @flow */
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
-import Reactotron from "../../ReactotronConfig";
 import rootReducer from "../reducers/rootReducer";
 
 export default function configureStore() {
   if (__DEV__) {
+    let Reactotron = require("../../ReactotronConfig").default;
     return Reactotron.createStore(rootReducer, applyMiddleware(thunk));
   } else return createStore(rootReducer, applyMiddleware(thunk));
 }
